4a0caeb8ede90ec1573df32cc5ac3c29a8ffb70f
sillyfunnypedro <sillyfunnypedro@gmail.com>
2024-04-04 10:34:14 -0700

    added javadoc

--- src/iteratelist/Main.java
+++ src/iteratelist/Main.java
@@ -5,6 +5,11 @@ package iteratelist;
  * This allows us to use the for-each loop to iterate through the collection.
  */
 public class Main {
+  /**
+   * Main entry point of the program.
+   *
+   * @param args The command line arguments.
+   */
   public static void main(String[] args) {
     RecyclableCollection recyclables = new RecyclableCollection();
     recyclables.addItem(new RecyclableItem("Bottle", true));
